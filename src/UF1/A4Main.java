785a73dddac73dfb481de71eed90408620c66ee7
jose <jbermejop01@elpuig.xeill.net>
2020-11-12 19:58:30 +0100

    commit

--- src/UF1/A4Main.java
+++ src/UF1/A4Main.java
@@ -1,6 +1,7 @@
 package UF1;
 
 import javax.crypto.SecretKey;
+import javax.security.auth.kerberos.EncryptionKey;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -8,29 +9,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 
 public class A4Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Xifrar xifrar = new Xifrar();
-
         File file = new File("/home/dam2a/Escriptori/clausA4.txt");
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String linea=bufferedReader.readLine();
-
-        Path path = Paths.get("/home/dam2a/Escriptori/clausA4.txt");
+        Path pathLinea = Paths.get(linea);
+        byte[] lineaEnBytes = Files.readAllBytes(pathLinea);
+        Path path = Paths.get("/home/dam2a/Escriptori/textamagat");
         byte[] textoenbytes = Files.readAllBytes(path);
+        String textoCifrado=new String(textoenbytes);
 
 
         while (linea != null){
             if (linea.equals("")){
                 linea=bufferedReader.readLine();
             }else {
-                SecretKey secretKey1 = xifrar.keygenKeyGeneration(128);
                 System.out.println("lectura: "+linea);
-                //xifrar.encryptData(linea.getBytes("UTF8"))
                 linea=bufferedReader.readLine();
 
             }
